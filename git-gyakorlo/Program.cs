0acddac68aa75f5a30524eccaee4ac780ccf65ca
Petrik Diak <diak@petrik.hu>
2024-01-31 13:22:20 +0100

    2.feladat: Lista létrehozása, számok hozzáadása felhasználótól bekérve.

--- git-gyakorlo/Program.cs
+++ git-gyakorlo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,39 @@ namespace git_gyakorlo
 	{
 		static void Main(string[] args)
 		{
+			//1.feladat:
 			//Létrehozva
+
+			//2.feladat:
+			List<int> szamok = new List<int>();
+
+            Console.Write("Adjon egy számot a listához: ");
+			int bekert = int.Parse(Console.ReadLine());
+
+			while (bekert != 0)
+			{
+				Console.Write("Adjon egy számot a listához: ");
+				bekert = int.Parse(Console.ReadLine());
+				szamok.Add(bekert);
+
+			}
+
+			
+
+			for (int i = 0; i < szamok.Count; i++)
+			{
+				Console.Write(szamok[i] + "");
+            }
+		
+
+
+			
+
+
+
+
+
+
 		}
 	}
 }
